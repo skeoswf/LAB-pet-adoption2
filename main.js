2b1482f1b8993bb3170f1c0ab776f39edb04aefd
Skeo <skeo051600@gmail.com>
2024-04-15 21:23:43 -0500

    Cards render to dom for each pet

--- main.js
+++ main.js
@@ -241,4 +241,19 @@ const pets = [
     }
   ];
 
-  console.log("i hate that im you")
+const app = document.getElementById("app");
+
+let domString = "";
+
+  pets.forEach((pet) => {
+    domString += `<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>`
+  });
+
+app.innerHTML = domString;
